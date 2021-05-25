5b112c62ce5eb142735950670a81eb3b334d5db2
Inaki Madrigal <iamadrig@calpoly.edu>
2021-05-24 18:25:36 -0700

    frontend change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-// import {API, Auth} from 'aws-amplify'
 import Amplify, {API, Auth} from 'aws-amplify'
 import { withAuthenticator } from '@aws-amplify/ui-react'
 
 function App() {
-  async function callApi() {
+  async function callHelloWorld() {
     const user = await Auth.currentAuthenticatedUser()
     const token = user.signInUserSession.idToken.jwtToken
     console.log({ token })
 
     const requestInfo = {
-      headers: {
-        Authorization: token
-      }
+      headers: { Authorization: token }
     }
 
-    const data = await API.get('forensicsrangeapi', '/forensicsrangeapi', requestInfo)
+    const data = await API.get('forensicsrangeapi', '/hello', requestInfo)
     console.log({ data })
   }
+
+  async function callTestMessage() {
+    const user = await Auth.currentAuthenticatedUser()
+    const token = user.signInUserSession.idToken.jwtToken
+    console.log({ token })
+
+    const requestInfo = {
+      headers: { Authorization: token }
+    }
+
+    const data = await API.get('forensicsrangeapi', '/test', requestInfo)
+    console.log({ data })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -26,7 +37,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={callApi}> Call Api </button>
+        <button onClick={callHelloWorld}> Call Hello World </button>
+        <button onClick={callTestMessage}> Call Test Message </button>
       </header>
     </div>
   );
