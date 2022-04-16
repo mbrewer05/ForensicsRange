8a070899ec9715658bd312b4608be66e4f36f5b1
cpshan <cody.33shan@gmail.com>
2022-04-16 12:27:55 -0700

    Changes to progress page

--- src/pages/Progress.js
+++ src/pages/Progress.js
@@ -2,22 +2,18 @@
 
 import React from "react";
 
-const Progress = () => (
-  <div>
-    <h1 className="title1">Progress</h1>
-    <p>
-      Progress PAGE
-    </p>
-    <p>
-        Scenario1
-    </p>
-    <p>
-        Scenario2
-    </p>
-    <p>
-        Scenario3
-    </p>
-  </div>
-);
+class Progress extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title1">Progress</h1>
+                <p>Progress PAGE</p>
+                <p>Scenario1</p>
+                <p>Scenario2</p>
+                <p>Scenario3</p>
+            </div>
+        );
+    }
+}
 
 export default Progress;
